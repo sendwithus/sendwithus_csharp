0e72b7c353e3bc328d866b5732875001713b6bfa
chennig <hennig.chris@gmail.com>
2016-07-07 11:06:36 -0700

    Added Send Email support, sendwithus caps changes
    Full support and testing for the Sending Emails api call

---
+++ Sendwithus/Model/Email.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Script.Serialization;
+
+namespace Sendwithus
+{
+    public class Email
+    {
+        public const string EMAIL_RESOURCE = "send";
+
+        public string template { get; set; } // The template ID
+        public Dictionary<string, object> template_data { get; }
+        public EmailRecipient recipient { get; set; }
+        public List<EmailRecipient> cc { get; }
+        public List<EmailRecipient> bcc { get; }
+        public EmailSender sender { get; set; }
+        public List<string> tags { get; }
+        public Dictionary<string, string> headers { get; }  // TODO: Confirm that this is the right representation for headers (looks like it is from the python library's usage)
+        public FileData inline { get; set; }   // TODO: is this the right representation of inline?  Should it be a list of FileData? Are other fields possibe beyond ID and Data?
+        public List<FileData> files { get; }
+        public string esp_account { get; set; }
+        public string locale { get; set; }
+        public string version_name { get; set; }
+
+        /// <summary>
+        /// Constructor for an email
+        /// </summary>
+        /// <param name="template">The template ID to send</param>
+        /// <param name="template_data">Object containing email template data</param>
+        /// <param name="recipient">The email recipient</param>
+        /// <param name="cc">An array of CC recipients</param>
+        /// <param name="bcc">An array of BCC recipients</param>
+        public Email(string template, Dictionary<string, object> template_data, EmailRecipient recipient)
+        {
+            this.template = template;
+            this.template_data = template_data;
+            this.recipient = recipient;
+            sender = new EmailSender();
+            cc = new List<EmailRecipient>();
+            bcc = new List<EmailRecipient>();
+            tags = new List<string>();
+            headers = new Dictionary<string, string>();
+            inline = new FileData();   // TODO: is this the right representation of inline?  Should it be a list of FileData? Are other fields possibe beyond ID and Data?
+            files = new List<FileData>();
+    }
+
+        /// <summary>
+        /// Send the given email.
+        /// POST /send
+        /// </summary>
+        /// <param name="email">The email to send</param>
+        /// <returns>A response indicating whether the message was sent successfully and a summary of the message</returns>
+        public async Task<EmailResponse> Send()
+        {
+            // Send the POST request
+            var resource = String.Format("{0}", EMAIL_RESOURCE);
+            var jsonResponse = await RequestManager.SendPostRequestAsync(resource, this);
+
+            // Convert the JSON result into an object
+            var serializer = new JavaScriptSerializer();
+            var response = serializer.Deserialize<EmailResponse>(jsonResponse);
+            return response;
+        }
+        
+
+        /// <summary>
+        /// A class to represent an email recepient
+        /// </summary>
+        public class EmailRecipient
+        {
+            public string address { get; set; }
+            public string name { get; set; }
+
+            /// <summary>
+            /// Constructor for a new email recipient
+            /// </summary>
+            /// <param name="address">The email address of the recipient</param>
+            /// <param name="name">The name of the recipient (optional)</param>
+            public EmailRecipient(string address, string name = "")
+            {
+                this.address = address;
+                this.name = name;
+            }
+        }
+
+        /// <summary>
+        /// A class to represent an email sender
+        /// </summary>
+        public class EmailSender
+        {
+            public string address { get; set; }
+            public string reply_to { get; set; }
+            public string name { get; set; }
+
+
+            /// <summary>
+            /// Default constructor for a new email sender
+            /// </summary>
+            public EmailSender()
+            {
+                address = String.Empty;
+                reply_to = String.Empty;
+                name = String.Empty;
+            }
+
+            /// <summary>
+            /// Constructor for a new email sender
+            /// </summary>
+            /// <param name="address">The email address of the sender</param>
+            /// <param name="reply_to">The reply-to email address of the sender</param>
+            /// <param name="name">The name of the sender</param>
+            public EmailSender(string address, string reply_to, string name)
+            {
+                this.address = address;
+                this.reply_to = reply_to;
+                this.name = name;
+            }
+        }
+
+        /// <summary>
+        /// A class to represent inline data for an email
+        /// </summary>
+        public class FileData
+        {
+            public string id;
+            public string data;
+
+            public FileData()
+            {
+                id = String.Empty;
+                data = String.Empty;
+            }
+
+            /// <summary>
+            /// Constructor for the file data
+            /// </summary>
+            /// <param name="id">The id of the file data</param>
+            /// <param name="data">The file data, as a base 64 encoded string</param>
+            public FileData(string id, string data)
+            {
+                this.id = id;
+                this.data = data;
+            }
+        }
+
+        public class EmailResponse
+        {
+            public bool success { get; set; }
+            public string status { get; set; }
+            public string receipt_id { get; set; }
+            public EmailSummary email { get; set; }
+    }
+
+        /// <summary>
+        /// A class to represent the email summary that's included in the response to the send email API call
+        /// </summary>
+        public class EmailSummary
+        {
+            public string name { get; set; }
+            public string version_name { get; set; }
+            public string locale { get; set; }
+        }
+
+
+    }
+}
---
+++ Sendwithus/SendwithusTest/Tests/EmailTest.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Sendwithus;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using System.Net;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SendwithusTest
+{
+    [TestClass]
+    public class EmailTest
+    {
+        private const string API_KEY_TEST = "test_3e7ae15aeb9b8a4b50bce7138c88d81c696edd0d";
+        private const string API_KEY_PRODUCTION = "live_3cb190a9c3df0defbd0c0ab56e34f3b1747eedfa";
+        private const string ESP_ACCOUNT = "esp_EsgkbqQdDg7F3ncbz9EHW7";
+        private const string DEFAULT_TEMPLATE_ID = "tem_yn2viZ8Gm2uaydMK9pgR2B";
+        private const string INVALID_TEMPLATE_ID = "invalid_template_id";
+        private const string DEFAULT_LOCALE = "en-US";
+        private const string DEFAULT_SENDER_EMAIL_ADDRESS = "sendwithus.test+sender@gmail.com";
+        private const string DEFAULT_REPLY_TO_EMAIL_ADDRESS = "sendwithus.test+replyto@gmail.com";
+        private const string DEFAULT_RECIPIENT_EMAIL_ADDRESS = "sendwithus.test+recipient@gmail.com";
+        private const string DEFAULT_CC_EMAIL_ADDRESS_1 = "sendwithus.test+cc.one@gmail.com";
+        private const string DEFAULT_CC_EMAIL_ADDRESS_2 = "sendwithus.test+cc.two@gmail.com";
+        private const string DEFAULT_BCC_EMAIL_ADDRESS_1 = "sendwithus.test+bcc.one@gmail.com";
+        private const string DEFAULT_BCC_EMAIL_ADDRESS_2 = "sendwithus.test+bcc.two@gmail.com";
+        private const string DEFAULT_EMAIL_NAME = "Chuck Norris";
+        private const string DEFAULT_SENDER_NAME = "Matt Damon";
+        private const string DEFAULT_TAG_1 = "tag1";
+        private const string DEFAULT_TAG_2 = "tag2";
+        private const string DEFAULT_TAG_3 = "tag3";
+        private const string DEFAULT_HEADER_NAME = "X-HEADER-ONE";
+        private const string DEFAULT_HEADER_VALUE = "header-value";
+        private const string DEFAULT_INLINE_ID = "cat.png";
+        private const string DEFAULT_INLINE_DATA = "{BASE_64_ENCODED_FILE_DATA}";
+        private const string DEFAULT_FILE_NAME_1 = "doct.txt";
+        private const string DEFAULT_FILE_NAME_2 = "stuff.zip";
+        private const string DEFAULT_FILE_DATA = "{BASE_64_ENCODED_FILE_DATA}";
+        private const string DEFAULT_VERSION_NAME = "New Version";
+
+        /// <summary>
+        /// Tests the API call POST /send with only the required email parameters set
+        /// </summary>
+        /// <returns>The asynchronous task</returns>
+        [TestMethod]
+        public async Task TestSendEmailWithOnlyRequiredParametersAsync()
+        {
+            Trace.WriteLine("POST /send");
+            Sendwithus.Sendwithus.ApiKey = API_KEY_PRODUCTION;
+
+            // Make the API call
+            var email = BuildBarebonesEmail();
+            var response = await email.Send();
+
+            // Validate the response
+            TestValidation.ValidateResponse(response);
+        }
+
+        /// <summary>
+        /// Tests the API call POST /send with all email parameters set
+        /// </summary>
+        /// <returns>The asynchronous task</returns>
+        [TestMethod]
+        public async Task TestSendEmailWithAllParametersAsync()
+        {
+            Trace.WriteLine("POST /send");
+            Sendwithus.Sendwithus.ApiKey = API_KEY_PRODUCTION;
+
+            // Construct the email
+            var email = BuildBarebonesEmail();
+
+            // Configure all of the optional data
+            email.cc.Add(new Email.EmailRecipient(DEFAULT_CC_EMAIL_ADDRESS_1, DEFAULT_EMAIL_NAME));
+            email.cc.Add(new Email.EmailRecipient(DEFAULT_CC_EMAIL_ADDRESS_2, DEFAULT_EMAIL_NAME));
+            email.bcc.Add(new Email.EmailRecipient(DEFAULT_BCC_EMAIL_ADDRESS_1, DEFAULT_EMAIL_NAME));
+            email.bcc.Add(new Email.EmailRecipient(DEFAULT_BCC_EMAIL_ADDRESS_2, DEFAULT_EMAIL_NAME));
+            email.sender.address = DEFAULT_SENDER_EMAIL_ADDRESS;
+            email.sender.reply_to = DEFAULT_REPLY_TO_EMAIL_ADDRESS;
+            email.sender.name = DEFAULT_SENDER_NAME;
+            email.tags.Add(DEFAULT_TAG_1);
+            email.tags.Add(DEFAULT_TAG_2);
+            email.tags.Add(DEFAULT_TAG_3);
+            email.headers.Add(DEFAULT_HEADER_NAME, DEFAULT_HEADER_VALUE);
+            email.inline.id = DEFAULT_INLINE_ID;
+            email.inline.data = DEFAULT_INLINE_DATA;
+            email.files.Add(new Email.FileData(DEFAULT_FILE_NAME_1, DEFAULT_FILE_DATA));
+            email.files.Add(new Email.FileData(DEFAULT_FILE_NAME_2, DEFAULT_FILE_DATA));
+            email.version_name = DEFAULT_VERSION_NAME;
+
+            // Make the API call
+            var response = await email.Send();
+
+            // Validate the response
+            TestValidation.ValidateResponse(response);
+        }
+
+        /// <summary>
+        /// Tests the POST /send API call with an invalid template ID
+        /// </summary>
+        /// <returns>The asynchronous task</returns>
+        [TestMethod]
+        public async Task TestSendEmailWithInvalidTemplateId()
+        {
+            Trace.WriteLine("POST /send with an invalid template ID");
+            Sendwithus.Sendwithus.ApiKey = API_KEY_TEST;
+
+            // Constuct the email
+            Email email = BuildBarebonesEmail();
+            email.template = INVALID_TEMPLATE_ID;
+            try
+            {
+                var response = await email.Send();
+                Assert.Fail("Failed to throw exception");
+            }
+            catch (SendwithusException exception)
+            {
+                // Make sure the response was HTTP 400 Bad Request 
+                TestValidation.ValidateException(exception, HttpStatusCode.BadRequest);
+            }
+        }
+
+        /// <summary>
+        /// Creates an email object with only the required parameters
+        /// </summary>
+        /// <returns></returns>
+        private Email BuildBarebonesEmail()
+        {
+            // Construct the template data
+            var templateData = new Dictionary<string, object>();
+            templateData.Add("first_name", "Chuck");
+            templateData.Add("last_name", "Norris");
+            templateData.Add("img", "http://placekitten.com/50/60");
+            var link = new Dictionary<string, string>();
+            link.Add("url", "https://www.sendwithus.com");
+            link.Add("text", "sendwithus!");
+            templateData.Add("link", link);
+
+            // Construct the sender and recipients
+            var recipient = new Email.EmailRecipient(DEFAULT_RECIPIENT_EMAIL_ADDRESS);
+            var cc = new List<Email.EmailRecipient>();
+            cc.Add(new Email.EmailRecipient(DEFAULT_CC_EMAIL_ADDRESS_1));
+            cc.Add(new Email.EmailRecipient(DEFAULT_CC_EMAIL_ADDRESS_2));
+            var bcc = new List<Email.EmailRecipient>();
+            bcc.Add(new Email.EmailRecipient(DEFAULT_BCC_EMAIL_ADDRESS_1));
+            bcc.Add(new Email.EmailRecipient(DEFAULT_BCC_EMAIL_ADDRESS_2));
+
+            // Construct and return the email
+            return new Email(DEFAULT_TEMPLATE_ID, templateData, recipient);
+        }
+    }
+}
