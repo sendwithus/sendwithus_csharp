da5fefde4117a8e1052d7a9aac2841431d7c514d
chennig <hennig.chris@gmail.com>
2016-07-10 06:23:05 -0700

    Made the SendwithusException class Serializable
    Recommend change from Visual Studio Code Analysis

--- SendWithUs/Exception/SendWithUsException.cs
+++ SendWithUs/Exception/SendWithUsException.cs
@@ -7,10 +7,20 @@ using System.Net;
 
 namespace Sendwithus
 {
+    /// <summary>
+    /// sendwithus SendwithusException class.
+    /// Used to return an exception when an API call fails.  Provides the HTTP status code and error message.
+    /// </summary>
+    [Serializable]
     public class SendwithusException : System.Exception
     {
         public HttpStatusCode StatusCode;
 
+        /// <summary>
+        /// Default constructor
+        /// </summary>
+        /// <param name="statusCode">The HTTP status code of the API call that triggered the exception</param>
+        /// <param name="message">The associated error message</param>
         public SendwithusException(HttpStatusCode statusCode, string message) : base(message)
         {
             StatusCode = statusCode;
